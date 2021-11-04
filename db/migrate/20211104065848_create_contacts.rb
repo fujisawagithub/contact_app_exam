cb131600816edc2d11e00c7053ddf2f7aa8df650
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-11-04 16:04:39 +0900

    migrate作り直し

--- db/migrate/20211104065848_create_contacts.rb
+++ db/migrate/20211104065848_create_contacts.rb
@@ -2,13 +2,8 @@ class CreateContacts < ActiveRecord::Migration[6.0]
   def change
     create_table :contacts do |t|
       t.string :name
-      t.string :
       t.string :email
-      t.string :
-      t.string :content
-      t.text :
-
-      t.timestamps
+      t.text :content
     end
   end
 end
